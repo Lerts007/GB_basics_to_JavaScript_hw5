5e06ecd37cd68f9c81cfa3156fd2136442307469
DIMA_w <oddff@mail.ru>
2023-08-08 16:38:29 +0300

    Task 2 completed

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ const numbers = {
   keyin6: 6,
   keyin7: 7,
 };
+console.log("--------------------------------------------------------------------");
+console.log("Задание 1");
+console.log("--------------------------------------------------------------------");
 
 for (const key in numbers) {
   if (numbers[key] >= 3) {
@@ -57,6 +60,39 @@ for (const key in numbers) {
 // ],
 // };
 // --------------------------------------------------------------------
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+console.log("--------------------------------------------------------------------");
+console.log("Задание 2");
+console.log("--------------------------------------------------------------------");
+
+console.log(`${"author"} = ` + post.author);
+console.log(`${"dislikes"} = ` + post.comments[0].rating.dislikes);
+console.log(`${"userId"} = ` + post.comments[1].userId);
+console.log(`${"text"} = ` + post.comments[1].text);
 
 // --------------------------------------------------------------------
 // Задание 3
